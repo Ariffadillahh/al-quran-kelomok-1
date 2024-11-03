fde5fa92465267631d5335fc5b0f8c6962a97395
Ariffadillahh <fadilaharif99@gmail.com>
2024-11-03 15:12:47 +0700

    update footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -84,7 +84,7 @@ const Footer = () => {
             <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
               © 2024{" "}
               <a href="https://flowbite.com/" className="hover:underline">
-                Kelopok 5
+                Kelopok 1
               </a>
               . All Rights Reserved.
             </span>
