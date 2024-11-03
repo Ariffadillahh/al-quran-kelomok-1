6702300c2dc0b37a21bb03aeec094ff7d714412b
Ariffadillahh <fadilaharif99@gmail.com>
2024-11-03 15:04:02 +0700

    nav update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,25 +1,32 @@
 "use client";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { HiMiniBars3BottomRight } from "react-icons/hi2";
 import { IoClose } from "react-icons/io5";
-import Logo from "../asset/logo.png"
+import Logo from "../asset/logo.png";
 import Image from "next/image";
 
 const Navbar = () => {
   const [isSticky, setIsSticky] = useState<boolean>(false);
-  const [togle, setTogle] = useState<boolean>(false);
-  const handeScroll = () => {
+  const [toggle, setToggle] = useState<boolean>(false);
+
+  const handleScroll = () => {
     if (window.scrollY > 30) {
       setIsSticky(true);
     } else {
       setIsSticky(false);
     }
   };
-  window.addEventListener("scroll", handeScroll);
 
-  const handleTogle = () => {
-    setTogle(!togle);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
   };
 
   return (
@@ -33,17 +40,22 @@ const Navbar = () => {
       >
         <div className="justify-between flex w-full md:px-16 pt-1 text-xl">
           <div className="md:w-1/2  font-semibold">
-            <Link href="/"> <Image src={Logo} alt="logo" className="w-16 md:w-20" /></Link>
+            <Link href="/">
+              {" "}
+              <Image src={Logo} alt="logo" className="w-16 md:w-20" />
+            </Link>
           </div>
           <div>
-            <button className="text-2xl p-3 md:hidden" onClick={handleTogle}>
-              {!togle ? <HiMiniBars3BottomRight /> : <IoClose />}
+            <button className="text-2xl p-3 md:hidden" onClick={handleToggle}>
+              {!toggle ? <HiMiniBars3BottomRight /> : <IoClose />}
             </button>
           </div>
           <div className="md:w-1/2 p-4 space-x-16 uppercase md:justify-end font-sans hidden md:flex md:items-center">
             <Link href="/juz">Juz</Link>
             <Link href="/surah">Surah</Link>
-            <a href="#" className="cursor-not-allowed">Sejarah</a>
+            <a href="#" className="cursor-not-allowed">
+              Sejarah
+            </a>
             <Link href="/about">
               <button className=" px-4 py-1.5 rounded-md text-base  bg-black hover:text-black hover:bg-transparent hover:border border-black text-white duration-200 ">
                 About
@@ -51,18 +63,44 @@ const Navbar = () => {
             </Link>
           </div>
         </div>
-        <div className={!togle ? ' bg-white absolute -left-[100%] transition-all duration-200' : ' bg-white absolute transition-all duration-200 left-3 rounded-lg mt-3 drop-shadow-lg'}>
+        <div
+          className={
+            !toggle
+              ? " bg-white absolute -left-[100%] transition-all duration-200"
+              : " bg-white absolute transition-all duration-200 left-3 rounded-lg mt-3 drop-shadow-lg"
+          }
+        >
           <Link href="/juz">
-            <button onClick={handleTogle} className="w-full ps-3 py-2 border-b text-start">Juz</button>
+            <button
+              onClick={handleToggle}
+              className="w-full ps-3 py-2 border-b text-start"
+            >
+              Juz
+            </button>
           </Link>
           <Link href="/surah">
-            <button onClick={handleTogle} className="w-full ps-3 py-2 border-b text-start">Surah</button>
+            <button
+              onClick={handleToggle}
+              className="w-full ps-3 py-2 border-b text-start"
+            >
+              Surah
+            </button>
           </Link>
           <a href="#" className="cursor-not-allowed">
-            <button onClick={handleTogle} className="w-full ps-3 py-2 border-b text-start">Sejarah</button>
+            <button
+              onClick={handleToggle}
+              className="w-full ps-3 py-2 border-b text-start"
+            >
+              Sejarah
+            </button>
           </a>
           <Link href="/about">
-            <button onClick={handleTogle} className="w-full ps-3 py-2 text-start">About</button>
+            <button
+              onClick={handleToggle}
+              className="w-full ps-3 py-2 text-start"
+            >
+              About
+            </button>
           </Link>
         </div>
       </div>
